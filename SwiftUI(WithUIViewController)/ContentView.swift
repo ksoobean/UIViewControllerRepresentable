5e67ad5ba91598737c05c67bdc2210929726a9d0
ksoobean <ksoobean@gmail.com>
2019-10-23 16:13:54 +0900

    Initial Commit

---
+++ SwiftUI(WithUIViewController)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI(WithUIViewController)
+//
+//  Created by master on 2019/10/23.
+//  Copyright © 2019 ksb. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
